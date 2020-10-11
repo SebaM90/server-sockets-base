33d75364b56e646aedda079443fb6aef05875dda
Sebastian <sebamorsaline@hotmail.com>
2020-10-10 21:27:44 -0300

    Inicio del proyecto

---
+++ index.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+// settings
+app.set('port', process.env.PORT || 3000);
+
+// static files
+app.use( express.static( path.join(__dirname, 'public') ) );
+
+// start server;;
+const server = app.listen(app.get('port'), () => {
+  console.log("====== SERVER ON PORT", app.get('port'), "=================");
+} );
+
+// start Socket
+const SocketIO = require('socket.io');
+const io = SocketIO(server);
+
+// array of clients
+let clients = [];
+
+/// webSockets
+io.on('connection', (socket) => {
+
+  // save client
+  clients.push(socket.id);
+
+  console.log("New user:",socket.id);
+
+  socket.on('disconnect', function() {
+      clients.splice(clients.indexOf(socket), 1);
+      io.emit('usuarios', clients);
+  });
+
+  io.emit('usuarios', clients );
+
+});
+
